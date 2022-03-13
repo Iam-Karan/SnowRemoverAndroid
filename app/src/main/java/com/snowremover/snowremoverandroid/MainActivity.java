15a2620f0c43aa9ddf5f572ea44eb8c22a277d5d
Karan Patel <75926283+iam-karan@users.noreply.github.com>
2022-03-13 19:27:56 -0400

    Add Bottom navigation - Create an empty home screen - add bottom navigation - add four fragments with view model - connect fragmnets with bottom navigation

--- app/src/main/java/com/snowremover/snowremoverandroid/MainActivity.java
+++ app/src/main/java/com/snowremover/snowremoverandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.snowremover.snowremoverandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(1*3000);
+
+                    Intent i=new Intent(getBaseContext(),HomeScreen.class);
+                    startActivity(i);
+
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        background.start();
     }
 }
---
+++ app/src/main/java/com/snowremover/snowremoverandroid/ui/order/OrderViewModel.java
@@ -0,0 +1,7 @@
+package com.snowremover.snowremoverandroid.ui.order;
+
+import androidx.lifecycle.ViewModel;
+
+public class OrderViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
