c8d0b079cdb4359ba3ee4e3636338cd34440dd7c
damanbehl <damanbehl@gmail.com>
2022-03-16 07:39:11 -0400

    Complete User profile screen - add logic on order screen for redirect on which screen - fetch name in order screen from database - connect User profile to database - add update name and update password functionality

--- app/src/main/java/com/snowremover/snowremoverandroid/UserProfileActivity.java
+++ app/src/main/java/com/snowremover/snowremoverandroid/UserProfileActivity.java
@@ -1,25 +1,75 @@
 package com.snowremover.snowremoverandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private ImageButton backButton, logoutButton;
+    private TextView email;
+    private EditText name, oldPasword, newPassword, confirmPassword;
+    private AppCompatButton updateBtn;
+    private FirebaseFirestore firestore;
+    private FirebaseUser mFirebaseUser;
+    private String uId;
+    private String nameText, updateNameText,  oldPasswordText, newPasswordText, confirmPasswordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        UserProfileActivity.this.setTitle("");
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        mFirebaseUser = mAuth.getCurrentUser();
+        assert mFirebaseUser != null;
+        uId = mFirebaseUser.getUid();
+
         getUi();
+        setData();
+
+        updateBtn.setOnClickListener(view -> {
+            updateNameText = name.getText().toString();
+            oldPasswordText = oldPasword.getText().toString();
+            newPasswordText = newPassword.getText().toString();
+            confirmPasswordText = confirmPassword.getText().toString();
+
+            if(!updateNameText.equals(nameText)){
+                UpdateName();
+            }
+            if(!oldPasswordText.isEmpty()){
+                ValidatePassword();
+            }
+        });
+
         backButton.setOnClickListener(view -> {
             Intent intent = new Intent(getBaseContext(), HomeScreen.class);
             startActivity(intent);
         });
         logoutButton.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(getBaseContext(), HomeScreen.class);
             startActivity(intent);
         });
@@ -28,5 +78,93 @@ public class UserProfileActivity extends AppCompatActivity {
     public void getUi(){
         backButton = findViewById(R.id.back_button);
         logoutButton = findViewById(R.id.logout_btn);
+        name = findViewById(R.id.user_name);
+        email = findViewById(R.id.user_email);
+        oldPasword = findViewById(R.id.user_old_password);
+        newPassword = findViewById(R.id.user_new_password);
+        confirmPassword = findViewById(R.id.user_confirm_password);
+        updateBtn = findViewById(R.id.user_update);
+    }
+
+    public void setData(){
+        firestore.collection("users").document(uId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                assert document != null;
+                if (document.exists()) {
+                    nameText = Objects.requireNonNull(document.getData().get("name")).toString();
+                    String emailText = Objects.requireNonNull(document.getData().get("email")).toString();
+
+                    name.setText(nameText);
+                    email.setText(emailText);
+
+                } else {
+                    Log.d("document Not Found", "No such document");
+                }
+            } else {
+                Log.d("error", "get failed with ", task.getException());
+            }
+        }).addOnFailureListener(e -> Log.d("error", e.toString()));
+
+    }
+
+    public void UpdateName(){
+        if(!updateNameText.isEmpty()){
+            firestore.collection("users").document(uId)
+                    .update("name", updateNameText)
+                    .addOnSuccessListener(unused -> {
+                        Toast.makeText(getApplicationContext(), "Name updated successful!", Toast.LENGTH_SHORT).show();
+                        setData();
+                    }).addOnFailureListener(e -> Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show());
+        }else {
+            name.setError("Name must not be empty!");
+        }
+    }
+
+    public void ValidatePassword(){
+        if(isValidPassword(newPasswordText)){
+            if(newPasswordText.equals(confirmPasswordText)){
+                UpdatePasword();
+            } else {
+                confirmPassword.setError("Confirm password not match!");
+            }
+        }else {
+            newPassword.setError("New password must not be empty!");
+        }
+    }
+
+    public static boolean isValidPassword(String password) {
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public void UpdatePasword(){
+        String emailText = email.getText().toString();
+        oldPasswordText = oldPasword.getText().toString();
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(emailText, oldPasswordText);
+
+        mFirebaseUser.reauthenticate(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        mFirebaseUser.updatePassword(newPasswordText)
+                                .addOnCompleteListener(task1 -> {
+                                    if(task1.isSuccessful()){
+                                         Toast.makeText(getBaseContext(), "Password updated successfully!", Toast.LENGTH_SHORT).show();
+                                         finish();
+                                         overridePendingTransition( 0, 0);
+                                         startActivity(getIntent());
+                                         overridePendingTransition( 0, 0);
+                                    } else {
+                                        Toast.makeText(getBaseContext(), "Error password not updated!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        oldPasword.setError("Wrong Password!");
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/snowremover/snowremoverandroid/ui/order/OrderFragment.java
+++ app/src/main/java/com/snowremover/snowremoverandroid/ui/order/OrderFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +17,17 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.snowremover.snowremoverandroid.HomeScreen;
 import com.snowremover.snowremoverandroid.R;
 import com.snowremover.snowremoverandroid.SignInScreen;
 import com.snowremover.snowremoverandroid.SignUpScreen;
 import com.snowremover.snowremoverandroid.UserProfileActivity;
+
+import java.util.Objects;
 
 public class OrderFragment extends Fragment {
 
@@ -28,6 +35,9 @@ public class OrderFragment extends Fragment {
     private LinearLayout userInfoLayout;
     private TextView userName;
     private ImageButton cartBtn;
+    private FirebaseUser mFirebaseUser;
+    private FirebaseFirestore firestore;
+    private String uId;
 
     public static OrderFragment newInstance() {
         return new OrderFragment();
@@ -37,10 +47,25 @@ public class OrderFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.order_fragment, container, false);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mAuth.getCurrentUser();
+        if(mFirebaseUser != null){
+            firestore = FirebaseFirestore.getInstance();
+            uId = mFirebaseUser.getUid();
+            setData();
+        }
+
         getUi(view);
+
         userInfoLayout.setOnClickListener(view1 -> {
-            Intent intent = new Intent(getContext(), SignInScreen.class);
-            startActivity(intent);
+            if(mFirebaseUser != null){
+                Intent intent = new Intent(getContext(), UserProfileActivity.class);
+                startActivity(intent);
+            }else {
+                Intent intent = new Intent(getContext(), SignInScreen.class);
+                startActivity(intent);
+            }
+
         });
         return view;
     }
@@ -56,6 +81,25 @@ public class OrderFragment extends Fragment {
         userInfoLayout = view.findViewById(R.id.user_profile_layout);
         userName = view.findViewById(R.id.order_user_name);
         cartBtn = view.findViewById(R.id.order_cart_btn);
+    }
+
+    public void setData(){
+        firestore.collection("users").document(uId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                assert document != null;
+                if (document.exists()) {
+                    String nameText = Objects.requireNonNull(document.getData().get("name")).toString();
+
+                    userName.setText(nameText);
+
+                } else {
+                    Log.d("document Not Found", "No such document");
+                }
+            } else {
+                Log.d("error", "get failed with ", task.getException());
+            }
+        }).addOnFailureListener(e -> Log.d("error", e.toString()));
     }
 
 }
