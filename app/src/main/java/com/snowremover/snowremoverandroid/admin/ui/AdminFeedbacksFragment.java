f8eb7f73363a513010664b950d37f960eb5a9062
GaganKKaur <gaganm454@gmail.com>
2022-04-15 11:02:24 -0400

    admin fragment class

--- app/src/main/java/com/snowremover/snowremoverandroid/admin/ui/AdminFeedbacksFragment.java
+++ app/src/main/java/com/snowremover/snowremoverandroid/admin/ui/AdminFeedbacksFragment.java
@@ -3,12 +3,24 @@ package com.snowremover.snowremoverandroid.admin.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.snowremover.snowremoverandroid.CartModel;
 import com.snowremover.snowremoverandroid.R;
+import com.snowremover.snowremoverandroid.admin.FeedbackAdapter;
+import com.snowremover.snowremoverandroid.admin.FeedbackModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +38,11 @@ public class AdminFeedbacksFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private ArrayList<FeedbackModel> feedbacks;
+    private ArrayList<FeedbackModel> copyFeedbacks;
+    private RecyclerView feedBackRecyclerView;
+    private FeedbackAdapter adapter;
+    FirebaseFirestore firestore;
     public AdminFeedbacksFragment() {
         // Required empty public constructor
     }
@@ -61,6 +78,48 @@ public class AdminFeedbacksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_feedbacks, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_feedbacks, container, false);
+        feedbacks = new ArrayList<>();
+        copyFeedbacks = new ArrayList<>();
+        firestore = FirebaseFirestore.getInstance();
+        feedBackRecyclerView = view.findViewById(R.id.admin_feedback_recyclerview);
+        setData();
+
+        return view;
+    }
+
+    private void setAdapter(){
+        adapter = new FeedbackAdapter(feedbacks, copyFeedbacks);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        feedBackRecyclerView.setLayoutManager(layoutManager);
+        feedBackRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        feedBackRecyclerView.setAdapter(adapter);
+    }
+
+    private void setData(){
+        firestore.collection("feedback").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (!queryDocumentSnapshots.isEmpty()) {
+
+                        List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot d : list) {
+
+                            String id = d.getId();
+                            String name = d.getData().get("name").toString();
+                            String email = d.getData().get("email").toString();
+                            String feedback = d.getData().get("message").toString();
+                            boolean isRead = (boolean) d.getData().get("read");
+
+                            FeedbackModel model = new FeedbackModel(id, name, email, feedback, isRead);
+                            if(!isRead){
+                                feedbacks.add(model);
+                            }
+                        }
+                        copyFeedbacks.addAll(feedbacks);
+                    } else {
+                        Toast.makeText(getContext(), "No data found in Database", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(e -> Toast.makeText(getContext(), "Fail to get the data.", Toast.LENGTH_SHORT).show())
+                .addOnCompleteListener(task -> setAdapter());
     }
 }
